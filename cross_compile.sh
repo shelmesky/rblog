da7ea7077e8135d99171bc82243232544c411d34
roy.lieu <roy.lieu@gmail.com>
2013-12-26 18:33:33 +0800

    修改cross_compile.sh，只编译main.go

--- cross_compile.sh
+++ cross_compile.sh
@@ -2,35 +2,35 @@
 
 case $1 in
 	windows_386)
-		CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o server_windows_x86.exe
+		CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o server_windows_x86.exe main.go
 		;;
 
 	windows_amd64)
-		CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o server_windows_x64.exe
+		CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o server_windows_x64.exe main.go
 		;;
 
 	linux_386)
-		CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o server_linux_x86
+		CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o server_linux_x86 main.go
 		;;
 
 	linux_amd64)
-		CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o server_linux_x64
+		CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o server_linux_x64 main.go
 		;;
 
 	freebsd_386)
-		CGO_ENABLED=0 GOOS=freebsd GOARCH=386 go build -o server_freebsd_x86
+		CGO_ENABLED=0 GOOS=freebsd GOARCH=386 go build -o server_freebsd_x86 main.go
 		;;
 
 	freebsd_amd64)
-		CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o server_freebsd_x64
+		CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o server_freebsd_x64 main.go
 		;;
 
 	darwin_386)
-		CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -o server_darwin_x86
+		CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -o server_darwin_x86 main.go
 		;;
 
 	darwin_amd64)
-		CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o server_darwin_x64
+		CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o server_darwin_x64 main.go
 		;;
 
 	*)
