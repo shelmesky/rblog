8b011ad79affe086f5fb299211aba0ff5e21f98f
roy.lieu <roy.lieu@gmail.com>
2014-01-10 12:40:48 +0800

    fix bug: 后台返回json格式，ajaxuploadfile无法正确解析的问题.

--- controllers/upload/upload.go
+++ controllers/upload/upload.go
@@ -15,6 +15,7 @@ type Sizer interface {
 type MessageBody struct {
 	Filename string
 	Filesize string
+	Count    int64
 }
 
 type UploadResult struct {
@@ -28,18 +29,22 @@ type UploadController struct {
 
 // file upload handler
 func (this *UploadController) Post() {
+	count, _ := this.GetInt("count")
 	file, info, err := this.GetFile("fileToUpload")
 	if err != nil {
 		beego.Critical(err)
+		return
 	}
 	file_size := file.(Sizer).Size()
-	beego.Info(file, info.Filename, file_size, info.Header)
 	buf := bytes.NewBufferString("")
 	fmt.Fprintf(buf, "%.1f KB", float64(file_size)/1024.0)
-	result := UploadResult{MessageBody{info.Filename, buf.String()}, ""}
+	result := UploadResult{MessageBody{info.Filename, buf.String(), count}, ""}
 	b, err := json.Marshal(&result)
 	if err != nil {
 		beego.Critical(err)
 	}
+	/* 客户端的ajaxuploadfile需要的类型为text/html */
+	this.Ctx.Output.Header("Content-Type", "text/html;charset=UTF-8")
 	this.Ctx.WriteString(string(b))
+
 }
