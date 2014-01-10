b879c81d24dbdf2fff7d02457a8174ef65e42daa
roy.lieu <roy.lieu@gmail.com>
2014-01-10 13:46:07 +0800

    增加上传文件是否为空和文件大小的判断

--- controllers/upload/upload.go
+++ controllers/upload/upload.go
@@ -29,13 +29,24 @@ type UploadController struct {
 
 // file upload handler
 func (this *UploadController) Post() {
+	/* 客户端的ajaxuploadfile需要的类型为text/html */
+	this.Ctx.Output.Header("Content-Type", "text/html;charset=UTF-8")
+
 	count, _ := this.GetInt("count")
 	file, info, err := this.GetFile("fileToUpload")
 	if err != nil {
 		beego.Critical(err)
+		this.Ctx.WriteString(`{"Error": "Not select file."}`)
 		return
 	}
+
 	file_size := file.(Sizer).Size()
+	// if filesize more than 64MB
+	if file_size > 67108864 {
+		this.Ctx.WriteString(`{"Error": "Filesize more than 64MB."}`)
+		return
+	}
+
 	buf := bytes.NewBufferString("")
 	fmt.Fprintf(buf, "%.1f KB", float64(file_size)/1024.0)
 	result := UploadResult{MessageBody{info.Filename, buf.String(), count}, ""}
@@ -43,8 +54,7 @@ func (this *UploadController) Post() {
 	if err != nil {
 		beego.Critical(err)
 	}
-	/* 客户端的ajaxuploadfile需要的类型为text/html */
-	this.Ctx.Output.Header("Content-Type", "text/html;charset=UTF-8")
+
 	this.Ctx.WriteString(string(b))
 
 }
