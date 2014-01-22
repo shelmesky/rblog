68be820e6b2ed2befc2f5e7fa0d4c2c5337e6355
roy.lieu <roy.lieu@gmail.com>
2014-01-22 15:22:08 +0800

    增加admin文件分页

--- controllers/admin/admin.go
+++ controllers/admin/admin.go
@@ -351,15 +351,88 @@ type AdminFileController struct {
 }
 
 func (this *AdminFileController) Get() {
+	page_id, _ := this.GetInt("page")
+	var limit int64 = 5
+
 	o := orm.NewOrm()
-	var upload_files []models.UploadFile
-	num, err := o.QueryTable(new(models.UploadFile)).All(&upload_files)
-	if err != nil {
+
+	upload_count, err := o.QueryTable(new(models.UploadFile)).Count()
+	if (page_id * limit) > upload_count {
 		utils.Error(err)
+		this.Abort("404")
 	}
 
-	this.Data["MaxFiles"] = num
-	this.Data["UploadFiles"] = upload_files
+	var uploads []*models.UploadFile
+	o.QueryTable(new(models.UploadFile)).Limit(limit, page_id*limit).OrderBy("-UploadTime").All(&uploads)
+
+	/* 开始处理分页 */
+
+	// 最大显示几页
+	max_per_page := 5
+
+	// 计算总的页数，如果不能被max_per_page整除，则加1页
+	var max_pages int
+	max_pages = (int(upload_count) / int(limit))
+	if (upload_count % limit) > 0 {
+		max_pages += 1
+	}
+
+	var upload_count_elements []int
+	// 默认从第0页开始
+	var start int = 0
+	var end int = 0
+
+	// 如果总页数大于5，则默认到第5页结束
+	// 否则到最大页数结束
+	if max_pages >= max_per_page {
+		end = max_per_page
+	} else {
+		end = max_pages
+	}
+
+	/*
+		如果当前页数，大于等于最大页数
+		就根据当前页数，算出当前页落在哪个区间
+		例如当前第7页，处于5~10这个区间
+	*/
+	if int(page_id) >= max_per_page {
+		current_five_page := int(page_id) / max_per_page
+		start = current_five_page * max_per_page
+		end = start + 5
+
+		/*
+			根据 总页数 % 最大显示页数 = 剩余的页数
+			如果有剩余的页数，则end等于剩余的页数
+			意味着页数不能被max_per_page整除
+
+			如果end大于最大的页数
+			说明已经达到末尾
+			应该根据remain_page_nums重新计算end
+		*/
+		if end > max_pages {
+			remain_page_nums := max_pages % max_per_page
+			if remain_page_nums > 0 {
+				end = start + remain_page_nums
+			}
+		}
+	}
+
+	for i := start; i < end; i++ {
+		upload_count_elements = append(upload_count_elements, i)
+	}
+
+	this.Data["UploadCountNums"] = upload_count
+	this.Data["UploadFiles"] = uploads
+	this.Data["UploadsCount"] = upload_count_elements
+	this.Data["CurrentUploadPage"] = page_id
+	this.Data["PrevUploadPage"] = page_id - 1
+	this.Data["NextUploadPage"] = page_id + 1
+	this.Data["MaxUploadPage"] = max_pages - 1
+	this.Data["MinUploadPage"] = 0
+
+	/* 结束处理分页 */
+
+	this.Data["MaxFiles"] = upload_count
 	this.Data["BlogUrl"] = utils.Site_config.BlogUrl
 
 	this.Data["xsrfdata"] = template.HTML(this.XsrfFormHtml())
