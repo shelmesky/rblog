d726352c027ad5406f383dadbdb327829503b26a
roy.lieu <roy.lieu@gmail.com>
2014-02-01 07:16:09 +0800

    增加百度统计帐号

---
+++ static/js/baidu.js
@@ -0,0 +1,2 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb700582369fe151f26d8e5b08ce5b696' type='text/javascript'%3E%3C/script%3E"));
